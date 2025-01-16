de91166e33c8e44d7a1107afe4dd39b6f55af183
xnorkeling0 <xnorkeling0@gmail.com>
2025-01-16 18:17:57 -0500

    added IBM hello World with printing expectation to console

---
+++ src/ibm_hello_world.py
@@ -0,0 +1,62 @@
+import os
+from qiskit import QuantumCircuit
+from qiskit.quantum_info import SparsePauliOp
+from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
+from qiskit_ibm_runtime import EstimatorV2 as Estimator
+from qiskit_ibm_runtime import QiskitRuntimeService
+ 
+
+# Create a new circuit with two qubits
+qc = QuantumCircuit(2)
+ 
+# Add a Hadamard gate to qubit 0
+qc.h(0)
+ 
+# Perform a controlled-X gate on qubit 1, controlled by qubit 0
+qc.cx(0, 1)
+ 
+# Return a drawing of the circuit using MatPlotLib ("mpl"). This is the
+# last line of the cell, so the drawing appears in the cell output.
+# Remove the "mpl" argument to get a text drawing.
+qc.draw("mpl")
+
+# Set up six different observables.
+ 
+observables_labels = ["IZ", "IX", "ZI", "XI", "ZZ", "XX"]
+observables = [SparsePauliOp(label) for label in observables_labels]
+
+token  = os.getenv('IBM_QUANTUM_TOKEN') # getting the custom env variable that stores my IBM token
+service = QiskitRuntimeService(channel="ibm_quantum", token=token)
+backend = service.least_busy(simulator=False, operational=True)
+ 
+# Convert to an ISA circuit and layout-mapped observables.
+pm = generate_preset_pass_manager(backend=backend, optimization_level=1)
+isa_circuit = pm.run(qc)
+ 
+isa_circuit.draw("mpl", idle_wires=False)
+
+# Construct the Estimator instance.
+ 
+estimator = Estimator(mode=backend)
+estimator.options.resilience_level = 1
+estimator.options.default_shots = 5000
+ 
+mapped_observables = [
+    observable.apply_layout(isa_circuit.layout) for observable in observables
+]
+ 
+# One pub, with one circuit to run against five different observables.
+job = estimator.run([(isa_circuit, mapped_observables)])
+ 
+# Use the job ID to retrieve your job data later
+print(f">>> Job ID: {job.job_id()}")
+print(f">>> Job status: {job.status()}")
+
+# This is the result of the entire submission.  You submitted one Pub,
+# so this contains one inner result (and some metadata of its own).
+#job_result = job.result()
+ 
+# This is the result from our single pub, which had six observables,
+# so contains information on all six.
+pub_result = job.result()[0]
+print(f"Expectation values: {pub_result.data.evs}")
