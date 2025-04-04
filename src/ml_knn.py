72c9552241e2e6c4739534ce289b9646441bc70e
xnorkeling0 <xnorkeling0@gmail.com>
2025-04-04 08:17:19 -0400

    Added step 2 to doc

--- src/ml_knn.py
+++ src/ml_knn.py
@@ -11,6 +11,8 @@ Reviews possible values from 1 to 4 stars:
 The choice is 1 if option_1 > option_2 else it is 2 (including the
 case where option_1 = option_2)
 
+Step #1: grab the following data from a spreadsheet
+
         option_1 option_2  choice
 case_2  4.5      3         1
 case_1  1        1.5       2
@@ -18,7 +20,7 @@ case_3  4        4         1
 query   3.5      2         ?
 
 
-Step #1: Normalized Options (no_<option_number>)
+Step #2: Normalize Options (no_<option_number>)
 
 no_1 = option_1/(option_1^2 + option_2^2)
 no_2 = option_2/(option_1^2 + option_2^2)
