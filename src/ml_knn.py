de92b1f67150598cade7526e5b53da0ebbd25f10
xnorkeling0 <xnorkeling0@gmail.com>
2025-04-04 08:15:23 -0400

    changed names in doc

--- src/ml_knn.py
+++ src/ml_knn.py
@@ -2,25 +2,27 @@
 Machine Learning Example with the KNN Method
 
 Study Case:
+-----------
 Train a model that can recognize the best option (e.g., best restaurants
 , hotel) given their rating from online users reviews.
 
 Reviews possible values from 1 to 4 stars:
 1, 1.5, 2, 2.5, 3, 3.5, 4
-The choice is 1 if Option_1 > Option_2 else it is 2 (including the
-case where Option_1 = Option_2)
+The choice is 1 if option_1 > option_2 else it is 2 (including the
+case where option_1 = option_2)
 
-        Option_1 Option_2  Choice
-Case_1  1        1.5       2
-Case_2  4.5      3         1
-Case_3  4        4         1
-Query   3.5      2         ?
+        option_1 option_2  choice
+case_2  4.5      3         1
+case_1  1        1.5       2
+case_3  4        4         1
+query   3.5      2         ?
 
 
-Step #1: Normalized Options (NOs)
+Step #1: Normalized Options (no_<option_number>)
 
-NO1 = Option_1/(Option_1^2 + Option_2^2)
-NO1 = Option_1/(Option_1^2 + Option_2^2)
+no_1 = option_1/(option_1^2 + option_2^2)
+no_2 = option_2/(option_1^2 + option_2^2)
+
 """
 
 
