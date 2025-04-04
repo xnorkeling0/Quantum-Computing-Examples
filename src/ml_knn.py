f138de164eb8aa8e485de98b57e508d527fe3452
xnorkeling0 <xnorkeling0@gmail.com>
2025-04-04 07:59:36 -0400

    Added ML file

---
+++ src/ml_knn.py
@@ -0,0 +1,15 @@
+"""
+Machine Learning Example with the KNN Method
+
+Study Case:
+Train a model that can recognize the best option (e.g., best restaurants
+, hotel) given their rating from online users reviews.
+The choice is 1 if Option_1 > Option_2 else it is 2 (including the
+case where Option_1 = Option_2)
+
+        Option_1 Option_2  Choice
+Case_1  0.34     0.70      2
+Case_2  0.51     0.49      1
+Case_3  0.50     0.50      1
+Query   0.48     0.4       ?
+"""
