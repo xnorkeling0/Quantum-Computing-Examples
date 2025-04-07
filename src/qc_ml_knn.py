ebd0e8c2751ad238789d235a005d7320b54ec023
xnorkeling0 <xnorkeling0@gmail.com>
2025-04-07 07:56:46 -0400

    added docs

--- src/qc_ml_knn.py
+++ src/qc_ml_knn.py
@@ -34,9 +34,79 @@ def get_dataset(db_path):
 Simplified Study Case (dataset):
 -----------------------------------
         option_1 option_2  choice(label)
-case_1  4.5      3         1
-case_2  1        1.5       2
+case_1  4.5      3         1 (option_1 is selected)
+case_2  1        1.5       0 (option_2 is selected)
 test    3.5      2         ?
+
+in general:
+-----------------------------------
+        option_1 option_2  choice(label)
+case_1  a        b         1
+case_2  c        d         0
+test    e        f         ?
+
+Quantum Machine Learning steps:
+    1.  a Specific quantum circuit is implemented using 4 qubits
+    (q0, q1, q2, q3)
+    2.  qubits are initialized into a state vector containing
+    training data (case_1 and case_2) and query data (test)
+    3.  Hadamart gate is applied on qubit 3 (hence qubits are entagled
+    and they can't ne computed separately)
+    4.  state vector size = 2^n (n: number of qubits) in this case
+    5.  in the state vector states are increasing values starting from zero 
+    6. to encode in a state vector the 6 numbers of our dataset (4.5, 3, 1, 1.5, 3.5, 2) of our 
+    case study example above or in general (a,b,c,d,e,f) we need 3 qubits (Q3,Q2,Q1)
+    (2^3 = 8 states = state vector size)
+    7.  this is a  state vector with 8 states:
+    [000, 001, 010, 011, 100, 101, 110, 111]
+    8. this is the states to dataset "amplitude" encoding, and to fit the 6 data points
+    to the 8 states, the query poings  (e,f) have been repeated
+    
+    Q3,Q2,Q1
+    000 -> a
+    001 -> b
+    010 -> c
+    011 -> d
+    100 -> e
+    101 -> f
+    110 -> e <- repeated
+    111 -> f <- repeated
+
+    9.  Also the labels (or choice) from our study case table must be encoded using an
+    additional qubit (Q0) which encodes labels 0 or 1 hence we need to expand the state vector
+    by executing the tensorial product between the state vector of (Q3,Q2,Q1) and Q0=[0,1] obtaining
+    a new state vector or 2^(4 qubits) = 16 states as below.
+
+    form table remember that 
+    (a,b) -> Q0=1
+    (c,d) -> Q0=0
+
+    Q3,Q2,Q1,Q0 -> dataset values
+    0000 -> 0
+    0001 -> a <- "a" is stored where Q0=1 (label 1)
+    0010 -> 0
+    0011 -> b
+    0100 -> c <- "c" is stored where Q0=0 (label 0)
+    0101 -> 0
+    0110 -> d
+    0111 -> 0
+    1000 -> 0|<- 1000 and 1001 are for e (when query (e,f) is labelled 1)
+    1001 -> e|
+    1010 -> 0|<-1010 and 1011 are for f (when query (e,f) is labelled 1)
+    1011 -> f|
+    1100 -> e| <- repeated (when query (e,f) is labelled 0)
+    1101 -> 0| 
+    1110 -> f| <- repeated (when query (e,f) is labelled 0)
+    1111 -> 0|
+
+    10. all the data values in the 5th column must be normalized by dividing
+    them by 2 to fit them into the state vector.
+
+    The constant value 2 ensures that the sum of the squares of all those values
+    adds up to 1 (because they are the result of applying an Hadamart gate to 4 qubits
+    which originates a 16x16 matrix multiplied by a 1/2 constant)
+
+
 """
 
 dataset = get_dataset(db_path)
