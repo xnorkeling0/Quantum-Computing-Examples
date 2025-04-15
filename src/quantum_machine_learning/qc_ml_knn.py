bd8b73c0d2402afdcdcb754391c77275705a1ec8
xnorkeling0 <xnorkeling0@gmail.com>
2025-04-14 23:00:19 -0400

    moved functions to data processing module

--- src/quantum_machine_learning/qc_ml_knn.py
+++ src/quantum_machine_learning/qc_ml_knn.py
@@ -14,39 +14,40 @@ from qiskit_ibm_runtime import QiskitRuntimeService, SamplerV2 as Sampler
 # Add the parent directory to the PYTHONPATH
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from utils.save_account import save_account, get_first_available_backend
+from data_processing import get_dataset, normalize_dataset, normalize_test_set
 
 
 db_path = str(Path("src") / "quantum_machine_learning" / "dataset.csv")
 
-def get_dataset(db_path):  # TODO:move to common module
-    df = pd.read_csv(db_path)
-    dataset = df.values.tolist()
-    print(f"The Dataset:\n{dataset}")
-    return dataset
-    # The dataset from src/dataset.csv:
-    # (It needs an empty row above the first data row)
-    # dataset = [
-    #     [4.5, 3, 1],
-    #     [1, 1.5, 2]
-    # ]
-
-def normalize_dataset(dataset: list):  # TODO:move to common module
-    for i in range(len(dataset)):
-        base = sqrt(dataset[i][0]**2 + dataset[i][1]**2)
-        dataset[i][0] = dataset[i][0]/base
-        dataset[i][1] = dataset[i][1]/base
-        vector_length = sqrt((dataset[i][0])**2 + (dataset[i][1])**2)
-        print(f"Vector {i + 1} length after normalization: {vector_length}")
-    return dataset
-
-def normalize_test_set(test_set: list):  # TODO:move to common module
-    base = sqrt(test_set[0]**2 + test_set[1]**2)
-    test_set[0] = test_set[0]/base
-    test_set[1] = test_set[1]/base
-    vector_length = sqrt((test_set[0])**2 + (test_set[1])**2)
-    print(f"Normalized test points:\n{test_set[0]}\n{test_set[1]}")
-    print(f"test set euclidian vector length: {vector_length}")
-    return test_set
+# def get_dataset(db_path):  # TODO:move to common module
+#     df = pd.read_csv(db_path)
+#     dataset = df.values.tolist()
+#     print(f"The Dataset:\n{dataset}")
+#     return dataset
+#     # The dataset from src/dataset.csv:
+#     # (It needs an empty row above the first data row)
+#     # dataset = [
+#     #     [4.5, 3, 1],
+#     #     [1, 1.5, 2]
+#     # ]
+# 
+# def normalize_dataset(dataset: list):  # TODO:move to common module
+#     for i in range(len(dataset)):
+#         base = sqrt(dataset[i][0]**2 + dataset[i][1]**2)
+#         dataset[i][0] = dataset[i][0]/base
+#         dataset[i][1] = dataset[i][1]/base
+#         vector_length = sqrt((dataset[i][0])**2 + (dataset[i][1])**2)
+#         print(f"Vector {i + 1} length after normalization: {vector_length}")
+#     return dataset
+# 
+# def normalize_test_set(test_set: list):  # TODO:move to common module
+#     base = sqrt(test_set[0]**2 + test_set[1]**2)
+#     test_set[0] = test_set[0]/base
+#     test_set[1] = test_set[1]/base
+#     vector_length = sqrt((test_set[0])**2 + (test_set[1])**2)
+#     print(f"Normalized test points:\n{test_set[0]}\n{test_set[1]}")
+#     print(f"test set euclidian vector length: {vector_length}")
+#     return test_set
 """
 Simplified Study Case (dataset):
 -----------------------------------
@@ -267,7 +268,7 @@ under this condition the numerator is counted
 """
 numerator = 0
 denominator = 0
-shots = 1
+shots = 50
 for i in range(shots):
     job = sampler.run([qc_transpiled])
     print(f"Job ID is {job.job_id()}")
