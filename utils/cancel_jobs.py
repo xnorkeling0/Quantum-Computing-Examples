ed71df20052e5449a79bf1fa7b73e3facbf547de
xnorkeling0 <xnorkeling0@gmail.com>
2025-04-17 19:31:15 -0400

    print cancelled jobs

--- utils/cancel_jobs.py
+++ utils/cancel_jobs.py
@@ -7,9 +7,16 @@ all_jobs = service.jobs()
 
 # Filter jobs with status "QUEUED"
 pending_jobs = [job for job in all_jobs if job.status() == "QUEUED"]
+cancelled_jobs = [job for job in all_jobs if job.status() == "CANCELLED"]
 
 # Print job IDs
-for job in pending_jobs:
-    print(f"Job ID: {job.job_id()}")
-    job.cancel()
+if pending_jobs:
+    for job in pending_jobs:
+        print(f"Job ID: {job.job_id()}")
+        job.cancel()
+
+if cancelled_jobs:
+    print("Cancelled jobs IDs:")
+    for job in cancelled_jobs:
+        print(f"{job.job_id()}")
 
