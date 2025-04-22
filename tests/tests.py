f97431cc0e50c04adc6947edeacf88bd5408be7c
xnorkeling0 <xnorkeling0@gmail.com>
2025-04-22 06:30:36 -0400

    fixed test to plot the matrix

--- tests/tests.py
+++ tests/tests.py
@@ -1,33 +1,17 @@
-import os
-import sys
 import matplotlib.pyplot as plt
 from src.quantum_machine_learning.qc_ml_knn import QuantumKnnModel
 
-# class TestQuantumMachineLearningModel:
-# 
-#     def test_compute_tensor_product(self):
-#         qml_model = QuantumKnnModel()
-#         H_tensor_I = qml_model.compute_tensor_product
-#         plt.imshow(H_tensor_I, cmap='viridis', interpolation='nearest')
-#         plt.colorbar(label="Matrix Values")
-#         plt.title("Visualization of H ⊗ I ⊗ I ⊗ I")
-#         plt.xlabel("Columns")
-#         plt.ylabel("Rows")
-#         plt.show()
-    
-qml_model = QuantumKnnModel()
-H_tensor_I = qml_model.compute_tensor_product()
-H_tensor_I.astype(float)
-# print(H_tensor_I)
-plt.imshow(H_tensor_I, cmap='viridis', interpolation='nearest')
-plt.colorbar(label="Matrix Values")
-plt.title("Visualization of H ⊗ I ⊗ I ⊗ I")
-plt.xlabel("Columns")
-plt.ylabel("Rows")
-plt.show()
-if __name__ == "__main__":
-    """
-    To run this script not as pytest but just as a python script
-    use: python -m tests.test
-    """
-    pass
+class TestQuantumMachineLearningModel:
+
+    def test_plot_tensor_product(self):
+        """
+        To visualize on screen the tensor product matrix
+        """
+        qml_model = QuantumKnnModel()
+        H_tensor_I = qml_model.compute_tensor_product()
+        plt.imshow(H_tensor_I, cmap='viridis', interpolation='nearest')
+        plt.colorbar(label="Matrix Values")
+        plt.title("Visualization of H ⊗ I ⊗ I ⊗ I")
+        plt.xlabel("Columns")
+        plt.ylabel("Rows")
+        plt.show()
