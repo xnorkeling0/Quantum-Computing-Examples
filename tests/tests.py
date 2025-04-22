bd438227ea2691d2acee23b97ce48a36fd551eb4
xnorkeling0 <xnorkeling0@gmail.com>
2025-04-22 05:34:47 -0400

    added test

--- tests/tests.py
+++ tests/tests.py
@@ -1,9 +1,8 @@
-"""Unit tests module"""
+from quantum_machine_learning.qc_ml_knn import QuantumKnnModel
 
-class TestProject:
-    """
-    It groups test methods.
-    """
+class TestQuantumMachineLearningModel:
 
-    def test_method(self):
-        pass
+    def test_compute_tensor_product(self):
+        qml_model = QuantumKnnModel()
+        H_tensor_I = qml_model.compute_tensor_product
+        print(H_tensor_I)
